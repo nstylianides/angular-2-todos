c81335b8dd660c3f16da830196c4cb8158e2c172
nstylianides <nstylianides@gmail.com>
2016-04-26 09:46:46 +0300

    initial

--- app/main.ts
+++ app/main.ts
@@ -2,5 +2,5 @@
  * Created by nstyladmin on 26/4/2016.
  */
 import {bootstrap} from 'angular2/platform/browser';
-import {DiDemo}    from './di_demo';
-bootstrap(DiDemo);
+import {TodoApp}   from './todo_app';
+bootstrap(TodoApp);
---
+++ app/todo.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by nstyladmin on 26/4/2016.
+ */
+// Declare an interaface for type safety
+export interface Todo {
+  text: string,
+  done: boolean
+}
